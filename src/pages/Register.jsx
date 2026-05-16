90368fde15c2dc0a4be34c565586924db3a01d14
Shay Fleming <shayfleming1@gmail.com>
2026-05-16 11:54:33 +0200

    Add sign-in and redirect to login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,15 @@
 import axios from 'axios'
 import React, { useActionState } from 'react'
+import { useNavigate } from "react-router";
 
 export default function Register() {
+
+const navigate = useNavigate();
 
   // Handles form action state
   const [state, formAction, isPending] =
     useActionState(submitHandler, {})
+
 
   // Runs when form submits
   async function submitHandler(prev, formData) {
@@ -27,8 +31,9 @@ export default function Register() {
     console.log(res.data);
     console.log(res.data.token);
 
-    // TODO:
+    // TODO:Done
     // redirect to login page
+    navigate("/login");
   }
 
   return (
