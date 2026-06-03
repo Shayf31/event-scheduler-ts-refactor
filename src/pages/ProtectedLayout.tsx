5bf7fc0ad98079fcefd79793c0cdafd56951f159
Shay Fleming <shayfleming1@gmail.com>
2026-06-03 15:03:51 +0200

    Convert ProtectedLayout to TypeScript

--- src/pages/ProtectedLayout.tsx
+++ src/pages/ProtectedLayout.tsx
@@ -1,33 +1,25 @@
 // Navigate redirects users to another route
 // Outlet renders the protected child route if access is allowed
-
 import { Navigate, Outlet } from "react-router";
 
 // ProtectedLayout component
 // This acts like a guard for protected pages
-export default function ProtectedLayout() {
-  
+export default function ProtectedLayout(): React.ReactElement {
   // Get token from localStorage
- // The token was saved when the user logged in
-  const token = JSON.parse(localStorage.getItem("token"));
+  // The token was saved when the user logged in
+  const token: string | null = localStorage.getItem("token");
 
   // If there is no token,
- // the user is considered logged out
+  // the user is considered logged out
   if (!token) {
-
     // Redirect logged-out users to login page
     return <Navigate to="/login" />;
   }
 
   // If token exists,
- // allow the protected route to render
- //
- // Outlet:
- // "show the child route here"
- //
- // Example protected children:
- // /profile
- // /create-event
-
+  // allow the protected route to render
+  //
+  // Outlet:
+  // "show the child route here"
   return <Outlet />;
 }
